a59730f1eb3da8a28cc2b4ac0a3fb1aa6b11ab91
shahadatrucse <shahadat.rucse@gmail.com>
2024-09-23 08:17:27 +0600

    updates

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <H1>The Wild Oasis</H1>;
+    </>
+  );
+}
+
+export default App;
